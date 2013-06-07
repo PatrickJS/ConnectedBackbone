0c1ccd231ac34fd54c428fb730207bb8d8d29d5d
gdi2290 <github@gdi2290.com>
2013-06-06 20:49:53 -0700

    add to collection

--- public/js/views/person.js
+++ public/js/views/person.js
@@ -12,14 +12,25 @@ var PersonView = Backbone.View.extend({
 var person = new Person();
 var personView = new PersonView({model: person});
 
-var people = new People();
+var people = new People([
+  {
+    name: 'Patrick',
+    age: 23
+  },
+  {
+    name: 'John Doe',
+    age: 55
+  },
+  {
+    name: 'Sally Doe',
+    age: 29
+  }
+  ]);
 people.add(person);
 
 var person2 = new Person({name:'Patrick', age:23});
 var personView2 = new PersonView({model: person2});
 
 people.add(person2);
-
-console.log(people);
 
 $('body').html(personView.el).append(personView2.el);
