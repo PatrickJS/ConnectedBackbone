b9ad34f1896c672c586a5123934b27afa5b4cbd9
gdi2290 <github@gdi2290.com>
2013-06-07 16:38:17 -0700

    checking events scope

--- public/js/views/task.js
+++ public/js/views/task.js
@@ -2,6 +2,11 @@ App.Views.Task = Backbone.View.extend({
   tagName: 'li',
   template: App.Template('task.template'),
   events: {
+    'click button.edit': 'editTask'
+  },
+  editTask: function() {
+    console.log(this.model);
+    alert('You\'re editing the task');
   },
   render: function() {
     var template = this.template(this.model.attributes);
