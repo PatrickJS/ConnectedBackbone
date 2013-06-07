b3802fea5fc2c5d994cdb7d28da6adcc8bb95875
gdi2290 <github@gdi2290.com>
2013-06-06 21:06:13 -0700

    create collection view

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,29 @@
+var people = new People([
+  {
+    name: 'Patrick',
+    age: 23
+  },
+  {
+    name: 'John Doe',
+    age: 55
+  },
+  {
+    name: 'Sally Doe',
+    age: 29
+  }
+  ]);
+
+console.log(people);
+
+var model = people.at(0);
+
+console.log('name: ',model.get('name'));
+console.log('occupation: ',model.get('occupation'));
+console.log(people.toJSON());
+
+model.set('occupation', 'Web Developer');
+
+console.log('occupation: ',model.get('occupation'));
+console.log(people.toJSON());
+
 var peopleView = new PeopleView({collection: people});
---
+++ public/js/views/people.js
@@ -0,0 +1,9 @@
+var PeopleView = Backbone.View.extend({
+  tagName: 'ul',
+  render: function() {
+    // filter through all items in a collection
+    // for each, create a new Person View
+    // append to root element
+
+  }
+});
