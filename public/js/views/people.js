f5b8fa8e62c9a2036c9d41a9b3eed98c0e670abb
gdi2290 <github@gdi2290.com>
2013-06-06 21:17:19 -0700

    create new view for each model in the collection

--- public/js/views/people.js
+++ public/js/views/people.js
@@ -5,10 +5,11 @@ var PeopleView = Backbone.View.extend({
   tagName: 'ul',
   render: function() {
     // filter through all items in a collection
-    // for each, create a new Person View
-    // append to root element
     this.collection.each(function(person) {
-      console.log(person);
+      // for each, create a new Person View
+      var personView = new PersonView({model: person});
+      console.log(personView.el);
     });
+    // append to root element
   }
 });
