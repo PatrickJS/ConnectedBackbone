4a8a2611653ed66dac73ef01a1c4846b4657e222
gdi2290 <github@gdi2290.com>
2013-06-06 20:01:32 -0700

    Finished ep4

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,14 @@ var Person = Backbone.Model.extend({
     age: 30,
     occupation: 'Worker'
   },
+  validate: function(attrs) {
+    if (attrs.age < 0) {
+      return 'Age must be positive, stupid!';
+    }
+    if (!attrs.name) {
+      return 'Every person should have a name!';
+    }
+  },
   work: function() {
     return this.get('name') + ' is working.';
   }
