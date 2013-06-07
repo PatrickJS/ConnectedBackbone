5565b74df97ec93f3bda57da277b44216303da12
gdi2290 <github@gdi2290.com>
2013-06-06 19:43:59 -0700

    Finished ep3

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,10 @@
-var Quiz = function(title) {
-  this.title = title;
-};
-
-
-var Person = function(config) {
-  this.name = config.name;
-  this.age = config.age;
-  this.occupation = config.occupation;
-};
-
-Person.prototype.work = function() {
-  return this.name + ' is working.';
-};
+var Person = Backbone.Model.extend({
+  defaults: {
+    name: 'John Doe',
+    age: 30,
+    occupation: 'Worker'
+  },
+  work: function() {
+    return this.get('name') + ' is working.';
+  }
+});
