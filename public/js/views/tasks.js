7db40be2a70b96cf9faa61743f1108faa5897f96
gdi2290 <github@gdi2290.com>
2013-06-07 15:57:12 -0700

    working collection view render

--- public/js/views/tasks.js
+++ public/js/views/tasks.js
@@ -1,8 +1,12 @@
 App.Views.Tasks = Backbone.View.extend({
   tagName: 'ul',
   render: function() {
-    this.collection.each(function(model) {
-
-    });
+    this.collection.each(this.addOne, this);
+  },
+  addOne: function(task) {
+    // creating a new child view
+    var taskView = new App.Views.Task({model: task});
+    // append to the root element
+    this.$el.append(taskView.render().el);
   }
 });
