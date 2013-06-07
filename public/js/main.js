ec01a87e1c337339f2c5ac3108580e399a4ad798
gdi2290 <github@gdi2290.com>
2013-06-06 19:43:48 -0700

    Finished ep2

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+var Quiz = function(title) {
+  this.title = title;
+};
+
+
+var Person = function(config) {
+  this.name = config.name;
+  this.age = config.age;
+  this.occupation = config.occupation;
+};
+
+Person.prototype.work = function() {
+  return this.name + ' is working.';
+};
