9b1066db6cc4510b564dfe7ade0dd19d2f14ab3c
gdi2290 <github@gdi2290.com>
2013-06-07 16:29:17 -0700

    click alert events test

--- public/js/views/task.js
+++ public/js/views/task.js
@@ -1,6 +1,13 @@
 App.Views.Task = Backbone.View.extend({
   tagName: 'li',
-
+  template: template('task.template'),
+  events: {
+    'click span': 'showAlert'
+  },
+  showAlert: function(e) {
+    alert('you clicked me!');
+    return false;
+  },
   render: function() {
     this.$el.html(this.model.get('title'));
     return this;
