6e0b902799636147076dd435c05a6b8780474933
gdi2290 <github@gdi2290.com>
2013-06-07 23:47:20 -0700

    start of ep18

--- public/js/main.js
+++ public/js/main.js
@@ -7,27 +7,10 @@
 
   App.Router = Backbone.Router.extend({
     routes: {
-      '': 'index',
-      'show/:id': 'show',
-      'download/*filename': 'download',
-      'search/:query': 'search',
-      '*pathname': 'defaultRoute'
+      '': 'index'
     },
     index: function() {
       console.log('in the index page');
-    },
-    show: function(id) {
-      console.log('show routes for id of: ' + id);
-    },
-    download: function(filename) {
-      console.log(filename);
-    },
-    search: function(query) {
-      console.log(query);
-    },
-    defaultRoute: function(pathname) {
-      alert('not sure what you mean here... ' + pathname);
-      console.log(pathname);
     }
 
   });
