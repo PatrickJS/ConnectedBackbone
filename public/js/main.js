843eeac42f6b078835cda9df82734cbc57a3a067
gdi2290 <github@gdi2290.com>
2013-06-07 23:39:14 -0700

    use :id with *filename

--- public/js/main.js
+++ public/js/main.js
@@ -8,13 +8,17 @@
   App.Router = Backbone.Router.extend({
     routes: {
       '': 'index',
-      'show/:id': 'show'
+      'show/:id': 'show',
+      'download/:id/*filename': 'download'
     },
     index: function() {
       console.log('in the index page');
     },
     show: function(id) {
       console.log('show routes for id of: ' + id);
+    },
+    download: function(id) {
+      console.log(id);
     }
   });
   new App.Router();
