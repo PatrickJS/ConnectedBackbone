083bdfba5b0cfbff07e8ce4d3685b45dca55d6c6
gdi2290 <github@gdi2290.com>
2013-06-07 23:57:47 -0700

    add vents and AppointmentView

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,17 @@
     Views: {}
   };
 
+  var vent = _.extend({}, Backbone.Events);
+
+  App.Views.Appointment = Backbone.View.extend({
+    initialize: function() {
+      vent.on('appointment:show', this.show, this);
+    },
+    show: function(id) {
+      console.log('showing the appointment with the id of: '+id);
+    }
+  });
+
   App.Router = Backbone.Router.extend({
     routes: {
       '': 'index',
@@ -14,11 +25,12 @@
       console.log('in the index page');
     },
     showAppointment: function(id) {
+      vent.trigger('appointment:show', id);
       console.log('appointment id: '+id);
     }
 
   });
-
+  new App.Views.Appointment();
   new App.Router();
   Backbone.history.start();
 
